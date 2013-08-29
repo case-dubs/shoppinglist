19d93aad38958d9af0b6be159e41c7baba754a93
case-dubs <casey594@gmail.com>
2013-08-29 16:10:15 -0700

    done

--- application.js
+++ application.js
@@ -1,5 +1,9 @@
 //constructing a listItem object
 
+function eraseContent (x){
+	x.value = " ";
+}
+
 function listItem (item, quantity) {
 	this.item = item;
 	this.quantity = quantity;
@@ -17,22 +21,25 @@ function submit() {
 	var quantityField = document.getElementById('quantity');
 	var quantityVal = quantityField.value;
 
-	var newEntry = new listItem;
-	newEntry.item = itemVal;
-	newEntry.quantity = quantityVal;
+	var newRow = document.createElement("tr");
+	
+	var newCell1 = document.createElement("td");
+	var itemCellVal = document.createTextNode(itemVal);
 
-	console.log(newEntry);
-	myList.push(newEntry);
-	
-	var addRow = function (){
-		//how do I acces the last row in the table and append the new row and cells to that?
-		var newRow = document.createElement("tr");
-		var newCell1 = document.createElement("td");
+	var checkBox = document.createElement("input");
+	checkBox.type = "checkbox";
+	newCell1.appendChild(checkBox);
+	newCell1.appendChild(itemCellVal);
+
+	var newCell2 = document.createElement("td");
+	var quantityCellVal = document.createTextNode(quantityVal);
+	newCell2.appendChild(quantityCellVal);
+
 		//var cellItem = document.createTextNode(myList.item);
-		newRow.appendChild(newCell1);
-		//newRow.appendChild()
-	}
-	
+	newRow.appendChild(newCell1);
+	newRow.appendChild(newCell2);
+	var table = document.getElementById("table");
+	table.appendChild(newRow);
 	
 };
 
@@ -40,14 +47,42 @@ function submit() {
 //how do i differentiate between the two submit types (add and updates)??
 
 function updates(){
-	if (document.forms[0].item.checked) {
-		console.log("checked!");	
-		//how to I access this checked item?
-		var checkedItem = document.forms[0].item.checked;
-		//checkedItem.remove();
+
+	var table = document.getElementById("table");
+//	for (var i = 0, row; row = table.rows[i]; i++) {
+	console.dir(table.rows);
+
+	var arrayCheck = new Array();
+	
+		for (var i = table.rows.length - 1; i >= 0 ; i--) {
+	
+
+		var row = table.rows[i];
+
+		console.dir(i + "top of loop");
+		var firstCell = row.cells[0];
+//		console.dir(firstCell);
+		var checkBox = row.cells[0].firstChild;
+
+		if (checkBox.localName == "input"){
+			console.dir(i + "after input");
+			if(checkBox.checked == true){
+				console.log(i + "about to remove");
+				arrayCheck[i] = true;
+				table.removeChild(row);
+			}
+			else{
+				arrayCheck[i] = false;
+			}
+		}
 	}
 };
 
+
+
+//ANSWERED!!!!
+//How do I remove checked items?
+//Once I've stored a new listItem, how do I add it to myList? !!! I'm lost on this!
 
 //How do I add new TRs (each with a check box, item and quantity values when a newEntry is added)? {I think I need to find the location in the dom and create a new tr - not sure how to do this, especially with the check box }
 
@@ -57,13 +92,5 @@ function updates(){
 /*if (document.forms[0].item.checked) {
 	user_input = document.forms[0].checkbox.name
 }*/
-
-
-//ANSWERED!!!!
-//How do I remove checked items?
-//Once I've stored a new listItem, how do I add it to myList? !!! I'm lost on this!
-
-console.log(myList);
-
 
 //How do I add check boxes next to my table items? ***ANSWERED!
